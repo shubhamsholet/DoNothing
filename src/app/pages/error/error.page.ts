238a7b08e565c05449beef052a8ed05ece94086c
Shubham Sholet <shubhamsholet@gmail.com>
2024-07-26 11:13:55 +0530

    2nd commit

--- src/app/pages/error/error.page.ts
+++ src/app/pages/error/error.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.page.html',
   styleUrls: ['./error.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCol, IonRow, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ErrorPage implements OnInit {
 
