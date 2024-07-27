6eeb84ae1e722d869c3366010bc186f013a3072d
Shubham Sholet <shubhamsholet@gmail.com>
2024-07-27 10:32:46 +0530

    user details

--- src/app/modals/user-details/user-details.component.ts
+++ src/app/modals/user-details/user-details.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonRow, IonCol, ModalController, IonButton, IonButtons } from "@ionic/angular/standalone";
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonRow, IonCol, ModalController, IonButton, IonButtons, IonItem } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
   standalone: true,
-  imports: [IonButtons, CommonModule, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, FormsModule, IonButton, IonToolbar, IonHeader, IonTitle]
+  imports: [IonItem, IonButtons, CommonModule, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, FormsModule, IonButton, IonToolbar, IonHeader, IonTitle, IonRow, IonCol]
 })
 export class UserDetailsComponent implements OnInit {
   @Input() userinfo: any;
